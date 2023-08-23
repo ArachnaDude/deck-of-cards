260309169c6849179eecb4da56b10ba98501aa97
Matt <cunning.distraction@googlemail.com>
2023-08-23 23:30:18 +0100

    add shuffle function

--- deck.js
+++ deck.js
@@ -3,25 +3,11 @@ class Deck {
     this.deckOfCards = [];
 
     const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
-    const cardValues = [
-      "Ace",
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      "Jack",
-      "Queen",
-      "King",
-    ];
+    const cards = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
 
     for (let i = 0; i < suits.length; i++) {
-      for (let j = 0; j < cardValues.length; j++) {
-        this.deckOfCards.push(`${cardValues[j]} of ${suits[i]}`);
+      for (let j = 0; j < cards.length; j++) {
+        this.deckOfCards.push(`${cards[j]} of ${suits[i]}`);
       }
     }
   }
@@ -33,7 +19,17 @@ class Deck {
     return this.deckOfCards;
   }
 
-  shuffle() {}
+  // returns a randomly shuffled deck of cards
+  // shuffling uses the Fisher-Yates sorting algorithm
+  shuffle() {
+    const { deckOfCards } = this;
+    for (let i = deckOfCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deckOfCards[i], deckOfCards[j]] = [deckOfCards[j], deckOfCards[i]];
+    }
+    return deckOfCards;
+  }
 }
 const cards = new Deck();
-console.log(cards.factoryDeck());
+console.log(cards.shuffle());
+// cards.shuffle();
